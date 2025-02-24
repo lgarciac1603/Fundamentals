edffcf765040091d6c43e26509177c67fd7b6ceb
lgarciac1603 <lfgc851@gmail.com>
2025-02-24 06:14:53 -0600

    guided lab 1 - Control structures

---
+++ guided_lab_1/controlStructures.cs
@@ -0,0 +1,20 @@
+﻿namespace Fundamentals.guided_lab_1
+{
+    class controlStructures
+    {
+        public static void testAge()
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if ( age >= 18 )
+            {
+                Console.WriteLine("You are elegible to vote");
+            }
+            else
+            {
+                Console.WriteLine("You are not elegible to vote");
+            }
+        }
+    }
+}
